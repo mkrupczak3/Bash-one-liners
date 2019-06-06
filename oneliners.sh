07e3e587da0163d61a7e4613e611c84a7579427c
mkrupczak3 <mkrupczak3@gatech.edu>
2019-06-06 11:00:53 -0400

    add codeautitor

--- oneliners.sh
+++ oneliners.sh
@@ -7,3 +7,6 @@
 #     Quickly go into the directory and add mp3 ID3 tags with the artist's name 
 #         and album name sourced from the folder directly containing the mp3's
 cd Speed\ of\ Doom/ && eyeD3 -a "James Paddock" -A "$(basename "$PWD")" *.mp3 && cd .. && ls -lah
+
+# Generate a text file containing filneame headers and text of all files recursively in a directory, ignoring binary files. 
+find . -type f -not -path '*/\.*' -exec grep -Il '.' {} \; | xargs awk 'FNR==1{print "--------------------------------------------------------------------------------"; print FILENAME; print "--------------------------------------------------------------------------------"; print ""; }{ print; print "" }' > allcode.txt
