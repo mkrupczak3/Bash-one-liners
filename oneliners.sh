1783df91687fc6ca7a771b0cbc8b6a4fda76da9d
mkrupczak3 <matthew@krupczak.org>
2019-11-29 22:05:48 -0500

    Update oneliners.sh
    Added one liner for converting raw images to jpegs

--- oneliners.sh
+++ oneliners.sh
@@ -10,3 +10,7 @@ cd Speed\ of\ Doom/ && eyeD3 -a "James Paddock" -A "$(basename "$PWD")" *.mp3 &&
 
 # Generate a text file containing filneame headers and text content of all files recursively in a directory, ignoring binary files. 
 find . -type f -not -path '*/\.*' -exec grep -Il '.' {} \; | xargs awk 'FNR==1{print "--------------------------------------------------------------------------------"; print FILENAME; print "--------------------------------------------------------------------------------"; print ""; }{ print; print "" }' > allcode.txt
+
+# Convert images from one format to another with multithreaded processing
+# https://drewlustro.com/blog/batch-convert-raw-images-to-jpeg-with-imagemagick-in-parallel
+find . -type f -iname '*.ARW' -print0 | xargs -0 -n 1 -P 4 -I {} convert -verbose -units PixelsPerInch {} -colorspace sRGB -resize 2560x2650 -set filename:new '%t-%wx%h' -density 72 -format JPG -quality 80 '%[filename:new].jpg'
